c40284c2d947e9c4f4de46882e7f834deb12e211
Nikita Marcius <nikita.marcius@gmail.com>
2021-05-17 14:45:47 +0300

    nuxt

---
+++ _nuxt/08c6988.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{269:function(n,e,t){"use strict";t.r(e);var l=t(2),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("\n    111111111\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
